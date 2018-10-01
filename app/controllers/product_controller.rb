0f5e512dffbae6219e86a617ac76463413e27881
jizampa <jizampa@gmail.com>
2018-09-30 19:35:07 -0500

    final commit

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,5 +2,9 @@ class ProductController < ApplicationController
   def index
 
     @products = Product.all
+    @productCount = Product.count
+    @average = Product.average(:price)
+    @totalStock = Product.sum(:stock_quantity)
+
   end
 end
